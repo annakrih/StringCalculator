bffd3dbd3f5587f5131ad2d5208991153c30e1d6
annakrih <annah14@ru.is>
2017-10-15 20:33:08 +0000

    1. testOneNumber test succeded (testEmptyString var komið í stíðasta committi, gleymdi að skrifa það í lýsinguna

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -4,10 +4,15 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+    
     @Test
     public void testEmptyString() {
         assertEquals(0, Calculator.add(""));
+    }
+    
+    @Test
+    public void testOneNumber() {
+        assertEquals(1, Calculator.add("1"));
     }
 }
 
